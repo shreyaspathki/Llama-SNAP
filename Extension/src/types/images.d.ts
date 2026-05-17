72b1af41e8d1f88c643a60fe994b866105829f8c
shreyas-pathki <shreyas0430@gmail.com>
2026-05-17 13:13:42 +0000

    Add image module declarations for TypeScript imports

---
+++ Extension/src/types/images.d.ts
@@ -0,0 +1,8 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+
+// Allow importing static asset URLs in TypeScript React components
+// Example: import logo from './assets/logosnap.png';
